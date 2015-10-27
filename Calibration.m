cc84674f581e623efb22206c80f51f83f7208bb6
unknown <yonis@VSM-PC-132.eed.ef.technion.ac.il>
2015-10-27 19:40:29 +0200

    First water-in-the-pipe version. There is a bug that causes the normal to be perpendicular to x-z plane

--- Calibration.m
+++ Calibration.m
@@ -1,21 +1,45 @@
-% 0 - some constants
+% 0 - some constants and bureaucracy
 
-    pathToRecordings = '..\networking\Windows\KinectNetwork\KinectNetwork\Recordings';
+    clear all; clc; close all;
+    addpath('PlaneFit');
+    pathToDepthImages = 'DepthImages';
     numOfCameras = 2;
-    kinect = imaq.VideoDevice('kinect',2);
-
+    
 % 1 - Load the chosen depth images
 
-    for i = 1:numOfCameras
-        fullPath = fullfile(pathToRecordings, num2str(i), '100.png');
-        depthMM{i} = imread(fullPath, 'PNG');
-        pointCloud{i} = depthToPointCloud(depthMM{i}, kinect);
-    end
-
+    depthImage1 = imread(fullfile(pathToDepthImages, 'Camera1.png'));
+    depthImage2 = imread(fullfile(pathToDepthImages, 'Camera2.png'));
+    
 % 2 - Convert the images to point clouds
+
+    pointCloud1 = depthToCloud(depthImage1);
+    pointCloud2 = depthToCloud(depthImage2);
+
+    pointCloud1 = pointCloud(pointCloud1);
+    pointCloud2 = pointCloud(pointCloud2);
+    
+    showPointCloud(pointCloud1);
+    figure; 
+    showPointCloud(pointCloud2);
 
 % 3 - extract the points corresponding to the planar regions
 
+    upperLeftCorner1 = struct('x', 170, 'y', 183);
+    lowerRightCorner1 = struct('x', 277, 'y', 347);
+    [rows, columns] = meshgrid( upperLeftCorner1.x : lowerRightCorner1.x, upperLeftCorner1.y : lowerRightCorner1.y);
+    plane1 = pointCloud1.select(rows(:), columns(:));
+    
+    upperLeftCorner2 = struct('x', 139, 'y', 212);
+    lowerRightCorner2 = struct('x', 257, 'y', 367);
+    [rows, columns] = meshgrid( upperLeftCorner2.x : lowerRightCorner2.x, upperLeftCorner2.y : lowerRightCorner2.y);
+    plane2 = pointCloud2.select(rows(:), columns(:));
+    
 % 4 - perform regresison to obtain the plane parameters
 
+    [n1,V1,p1] = affine_fit(plane1.Location);
+    [n2,V2,p2] = affine_fit(plane2.Location);
+
 % 5 - from the plane parameters deduce the relative rotation and translation between the cameras
+
+    angle = acosd(dot(n1,n2));
+    disp(['The angle between the planes is: ', num2str(angle)]);
