8162621342dcb8a9c34f2ec231f06ffedef5c5e6
unknown <yonis@VSM-PC-132.eed.ef.technion.ac.il>
2015-10-28 17:19:28 +0200

    First working version of calibration code - finds angle between the two cameras

--- Calibration.m
+++ Calibration.m
@@ -16,24 +16,28 @@
     pointCloud2 = depthToCloud(depthImage2);
 
     pointCloud1 = pointCloud(pointCloud1);
-    pointCloud2 = pointCloud(pointCloud2);
-    
-    showPointCloud(pointCloud1);
-    figure; 
-    showPointCloud(pointCloud2);
+    pointCloud2 = pointCloud(pointCloud2);   
 
 % 3 - extract the points corresponding to the planar regions
+
+    roi = [ -inf, inf;   % x
+            -inf, inf;   % y
+              1 ,  2 ];  % z
 
     upperLeftCorner1 = struct('x', 170, 'y', 183);
     lowerRightCorner1 = struct('x', 277, 'y', 347);
     [rows, columns] = meshgrid( upperLeftCorner1.x : lowerRightCorner1.x, upperLeftCorner1.y : lowerRightCorner1.y);
     plane1 = pointCloud1.select(rows(:), columns(:));
+    plane1 = plane1.select(plane1.findPointsInROI(roi));
     
     upperLeftCorner2 = struct('x', 139, 'y', 212);
     lowerRightCorner2 = struct('x', 257, 'y', 367);
     [rows, columns] = meshgrid( upperLeftCorner2.x : lowerRightCorner2.x, upperLeftCorner2.y : lowerRightCorner2.y);
     plane2 = pointCloud2.select(rows(:), columns(:));
+    plane2 = plane2.select(plane2.findPointsInROI(roi));
     
+    showPointCloud(plane1); hold on;
+    showPointCloud(plane2);
 % 4 - perform regresison to obtain the plane parameters
 
     [n1,V1,p1] = affine_fit(plane1.Location);
@@ -42,4 +46,5 @@
 % 5 - from the plane parameters deduce the relative rotation and translation between the cameras
 
     angle = acosd(dot(n1,n2));
+    angle = min(angle, 180 - angle);
     disp(['The angle between the planes is: ', num2str(angle)]);
