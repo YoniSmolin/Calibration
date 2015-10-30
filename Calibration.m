3c5e3450a9bccdb7e5642016cd89b359c6c006a3
Yoni Smolin <Yoni.Smolin@gmail.com>
2015-10-30 19:41:00 +0200

    Correct translation and preform point cloud merge

--- Calibration.m
+++ Calibration.m
@@ -7,8 +7,8 @@
     
 % 1 - Load the chosen depth images
 
-    depthImage1 = imread(fullfile(pathToDepthImages, 'Camera1.png'));
-    depthImage2 = imread(fullfile(pathToDepthImages, 'Camera2.png'));
+    depthImage1 = fliplr(imread(fullfile(pathToDepthImages, 'Camera1.png')));
+    depthImage2 = fliplr(imread(fullfile(pathToDepthImages, 'Camera2.png')));
     
 % 2 - Convert the images to point clouds
 
@@ -24,14 +24,14 @@
             -inf, inf;   % y
               1 ,  2 ];  % z
 
-    upperLeftCorner1 = struct('x', 170, 'y', 183);
-    lowerRightCorner1 = struct('x', 277, 'y', 347);
+    upperLeftCorner1 = struct('x', 239, 'y', 199);
+    lowerRightCorner1 = struct('x', 343, 'y', 358);
     [columns, rows] = meshgrid( upperLeftCorner1.x : lowerRightCorner1.x, upperLeftCorner1.y : lowerRightCorner1.y);
     plane1 = pointCloud1.select(rows(:), columns(:));
 %     plane1 = plane1.select(plane1.findPointsInROI(roi));
     
-    upperLeftCorner2 = struct('x', 139, 'y', 212);
-    lowerRightCorner2 = struct('x', 257, 'y', 367);
+    upperLeftCorner2 = struct('x', 242, 'y', 202);
+    lowerRightCorner2 = struct('x', 380, 'y', 362);
     [columns, rows] = meshgrid( upperLeftCorner2.x : lowerRightCorner2.x, upperLeftCorner2.y : lowerRightCorner2.y);
     plane2 = pointCloud2.select(rows(:), columns(:));
 %     plane2 = plane2.select(plane2.findPointsInROI(roi));
@@ -51,12 +51,20 @@
     
 % 6 - Use a corner of the board to determine the relative translation
 
-    upperRightCorner1 = struct('x', 281, 'y', 179);
-    upperRightCorner2 = struct('x', 279, 'y', 183);
+    upperRightCorner1 = struct('x', 232, 'y', 180);
+    upperRightCorner2 = struct('x', 233, 'y', 184);
     
     xyz1 = pointCloud1.select(upperRightCorner1.y, upperRightCorner1.x);
     xyz2 = pointCloud2.select(upperRightCorner2.y, upperRightCorner2.x);
     
-    R = vrrotvec2mat([cross(n1,n2); deg2rad(angle)]);
+    R = vrrotvec2mat([cross(n1,n2); deg2rad(-angle)]);
     
-    disp(['The distance between the planes is: ', num2str((xyz2.Location' - R*xyz1.Location')'), ' [m]']);
+    translation = xyz2.Location*R - xyz1.Location;    
+    disp(['The distance between the planes is: ', num2str(translation), ' [m]']);
+    
+% 7 - Merge the two point clouds
+
+    tform = affine3d( [horzcat(R, zeros(3,1)) ; [ -translation 1] ] );
+    mergedPointCloud = pcmerge(pctransform(pointCloud2, tform), pointCloud1, 0.1);
+    figure; showPointCloud(mergedPointCloud);
+    xlabel('x'); ylabel('y'); zlabel('z');
